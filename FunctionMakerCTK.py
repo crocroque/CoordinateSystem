9e56ffeb41242c1a70e197b0c0e67d72efa68394
crocroque <118309728+crocroque@users.noreply.github.com>
2024-10-31 18:21:21 +0100

    Update FunctionMakerCTK.py

--- FunctionMakerCTK.py
+++ FunctionMakerCTK.py
@@ -1,9 +1,7 @@
 import customtkinter as ctk
 from tkinter import messagebox, colorchooser
-from Function.FunctionVisualizer import CoordinateSystem, FunctionEvaluatingError
+from FunctionVisualizer import CoordinateSystem, FunctionEvaluatingError
 import math
-
-ctk.set_default_color_theme("green")
 
 
 def show_function():
@@ -39,12 +37,13 @@ def show_function():
             system.show()
 
     except FunctionEvaluatingError as error:
-        messagebox.showerror(title="function error", message=error.message)
+        messagebox.showerror(title="Function Error", message=error.message)
         return
 
     except Exception as error:
         messagebox.showerror(title="Setting Error", message=error)
         return
+
 
 def make_param_entry(win: ctk.CTk, param_name: str, row, description: str, default_value: str) -> ctk.CTkEntry:
     ctk.CTkLabel(win, text=param_name).grid(row=row, column=0)
@@ -55,19 +54,21 @@ def make_param_entry(win: ctk.CTk, param_name: str, row, description: str, defau
     e.grid(row=row, column=1)
     return e
 
+
 def ask_color(entry: ctk.CTkEntry):
-    color_code = colorchooser.askcolor(title="Choose color")
+    color_code = colorchooser.askcolor(title="Choose Color")
     entry.delete(0, ctk.END)
     entry.insert(0, str(color_code[0]))
 
 
 def make_param_color_entry(win: ctk.CTk, param_name: str, row, default_value: str) -> ctk.CTkEntry:
     ctk.CTkLabel(win, text=param_name).grid(row=row, column=0)
+
     e = ctk.CTkEntry(win)
     e.grid(row=row, column=1)
     e.insert(0, default_value)
 
-    ctk.CTkButton(win, text="choose color", command=lambda: ask_color(e)).grid(row=row, column=2)
+    ctk.CTkButton(win, text="Choose Color", command=lambda: ask_color(e)).grid(row=row, column=2)
     return e
 
 
@@ -76,7 +77,7 @@ def make_param_check_box(win: ctk.CTk, param_name: str, row, description: str) -
     ctk.CTkLabel(win, text=description).grid(row=row, column=2)
 
     var = ctk.BooleanVar()
-    check_btn = ctk.CTkCheckBox(win, variable=var, onvalue=True, offvalue=False, text="")
+    check_btn = ctk.CTkCheckBox(win, variable=var, onvalue=True, offvalue=False)
     check_btn.grid(row=row, column=1)
     return check_btn, var
 
@@ -84,14 +85,22 @@ def make_param_check_box(win: ctk.CTk, param_name: str, row, description: str) -
 def automatic_tracestep():
     try:
         points_number = (float(x_max_entry.get()) - float(x_min_entry.get())) / float(trace_step_entry.get())
+        text = f"{round(points_number)} points"
+        if points_number > 100000:
+            text += " (may be long; try to increase the trace step)"
+
+        automatic_tracestep_label.configure(text=text)
 
     except:
         pass
 
-    else:
-        automatic_tracestep_label.configure(text=f"{points_number} points")
-
     root.after(100, automatic_tracestep)
+
+
+def make_win_title():
+    title = f"Visualization of f(x) = {function_entry.get()} ∀ x ∈ [{x_min_entry.get()} ; {x_max_entry.get()}]"
+    win_title_entry.delete(0, ctk.END)
+    win_title_entry.insert(0, title)
 
 
 def show_more_option():
@@ -101,41 +110,46 @@ def show_more_option():
     show_function_btn.grid(row=20, column=1)
 
     more_option = True
-    bg_color_entry = make_param_color_entry(root, "background color", 13, "(255, 255, 255)")
-    point_color_entry = make_param_color_entry(root, "points color", 14, "(0, 0, 0)")
-    axes_color_entry = make_param_color_entry(root, "axes color", 15, "(0, 0, 0)")
-    graduation_color_entry = make_param_color_entry(root, "graduation color", 16, "(0, 0, 0)")
-    show_coordinate_param = make_param_check_box(root, "show coordinate ?", 17, "Display the mouse coor at the top left")
-    win_title_entry = make_param_entry(root, "window title", 18, "", "function")
-    show_ignored_error_param = make_param_check_box(root, "show ignored error ?", 19, "Display the ignored error while calculating the points")
+    bg_color_entry = make_param_color_entry(root, "Background Color", 13, "(255, 255, 255)")
+    point_color_entry = make_param_color_entry(root, "Points Color", 14, "(0, 0, 0)")
+    axes_color_entry = make_param_color_entry(root, "Axes Color", 15, "(0, 0, 0)")
+    graduation_color_entry = make_param_color_entry(root, "Graduation Color", 16, "(0, 0, 0)")
+    show_coordinate_param = make_param_check_box(root, "Show Coordinates?", 17, "Display the mouse coordinates at the top left")
+    win_title_entry = make_param_entry(root, "Window Title", 18, "", "Function")
+    ctk.CTkButton(root, text="Automatic Title", command=make_win_title).grid(row=18, column=2)
+    show_ignored_error_param = make_param_check_box(root, "Show Ignored Errors?", 19, "Display ignored errors during calculations")
 
+
+# Initialize CTk window
+ctk.set_appearance_mode("System")  # Modes: "System", "Dark", "Light"
+ctk.set_default_color_theme("blue")  # Themes: "blue", "green", "dark-blue"
 
 root = ctk.CTk()
 root.title("Function Maker")
 root.resizable(False, False)
 
-win_width_entry = make_param_entry(root, "window_width", 0, "float > 0", "500")
-win_height_entry = make_param_entry(root, "window_height", 1, "float > 0", "500")
+win_width_entry = make_param_entry(root, "Window Width", 0, "float > 0", "500")
+win_height_entry = make_param_entry(root, "Window Height", 1, "float > 0", "500")
 
-function_entry = make_param_entry(root, "f(x) =", 2, "python syntax (math library can be used)", "")
+function_entry = make_param_entry(root, "f(x) =", 2, "Python syntax (math library can be used)", "")
 x_min_entry = make_param_entry(root, "x_min", 3, "float", "-10")
 x_max_entry = make_param_entry(root, "x_max", 4, "float", "10")
-x_graduation_step_entry = make_param_entry(root, "x_graduation_step", 5, "float > 0 | 0 for no graduation", "1")
+x_graduation_step_entry = make_param_entry(root, "x Graduation Step", 5, "float > 0 | 0 for no graduation", "1")
 
 y_min_entry = make_param_entry(root, "y_min", 6, "float", "-10")
 y_max_entry = make_param_entry(root, "y_max", 7, "float", "10")
-y_graduation_step_entry = make_param_entry(root, "y_graduation_step", 8, "float > 0 | 0 for no graduation", "1")
+y_graduation_step_entry = make_param_entry(root, "y Graduation Step", 8, "float > 0 | 0 for no graduation", "1")
 
-trace_step_entry = make_param_entry(root, "trace_step", 9, "float > 0", "0.1")
+trace_step_entry = make_param_entry(root, "Trace Step", 9, "", "0.1")
 automatic_tracestep_label = ctk.CTkLabel(root, text="")
 automatic_tracestep_label.grid(row=9, column=2)
 automatic_tracestep()
 
-draw_points_param = make_param_check_box(root, "draw_points ?", 10, "might need lower trace_step | accurate")
+draw_points_param = make_param_check_box(root, "Draw Points?", 10, "Might need lower trace step | accurate")
 draw_points_check_box = draw_points_param[0]
-draw_points_check_box.select()
+draw_points_check_box.select()  # Check the box
 
-draw_lines_between_points_param = make_param_check_box(root, "draw_lines_between_points ?", 11, "less accurate | might be faster")
+draw_lines_between_points_param = make_param_check_box(root, "Draw Lines Between Points?", 11, "Less accurate | might be faster")
 draw_lines_between_points_check_box = draw_lines_between_points_param[0]
 
 more_option = False
